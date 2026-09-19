8c67b2d5147468ffbd7008eb65a3ecbd0264f7f2
hugo <hugo.resennde1123@gmail.com>
2026-09-19 20:02:03 -0300

    feat: termino da comparacao

--- src/comparacoes/main.c
+++ src/comparacoes/main.c
@@ -46,22 +46,137 @@ int PesquisaBinaria(int * V, int N, int Chave, int *count) {
 }
 
 int main(){
-  int * v_a = (int *) malloc (VETOR_A * sizeof(int));
-  Gerar(v_a, VETOR_A);
-  int tamanho = VETOR_A;
-  int target[6] = {0,24999,49999,74999,99999,-1};
+  /* int * v_a = (int *) malloc (VETOR_A * sizeof(int)); */
+  /* Gerar(v_a, VETOR_A); */
+  /* int tamanho = VETOR_A; */
+  /* int target[6] = {0,24999,49999,74999,99999,-1}; */
+  /* int encontrou=0; */
+  /* int comparacoes = 0; */
+
+  /* merge_sort(v_a,tamanho); */
+
+  /* imprimir_array(v_a, VETOR_A); */
+
+  /* printf("=========== PESQUISAS SEQUENCIAIS==============\n\n\n"); */
+
+  /* for(int i =0; i<6;i++){ */
+  /*   clock_t inicio_sequencial = clock(); */
+  /*   encontrou = PesquisaSequencial(v_a, VETOR_A, target[i], &comparacoes); */
+  /*   clock_t termino_sequencial = clock(); */
+
+  /*   printf("==TERMINO DA PESQUISA== \n"); */
+  /*   printf("numero de comparacoes (sequencial)= %d\n", comparacoes); */
+  /*   if(encontrou != -1) printf("elemento encontrado\nindice = %d\n",encontrou); */
+  /*   else printf("elemento nao encontrado\n"); */
+
+  /*   double tempo_total_sequencial = ((double) termino_sequencial-inicio_sequencial)/CLOCKS_PER_SEC; */
+
+  /*   printf("Inicio: %.3lf\nTermino: %.3lf\nTempo de execucao: %.3lf\n\n\n",(double)inicio_sequencial/CLOCKS_PER_SEC, (double)termino_sequencial/CLOCKS_PER_SEC, tempo_total_sequencial); */
+
+  /*   comparacoes = 0; */
+
+  /* } */
+
+  /* printf("=========== PESQUISAS BINARIA==============\n\n\n"); */
+
+  /* comparacoes = 0; */
+
+  /* for(int i =0; i<6;i++){ */
+
+  /*   clock_t inicio_binaria = clock(); */
+  /*   encontrou = PesquisaBinaria(v_a, VETOR_A, target[i], &comparacoes); */
+  /*   clock_t termino_binaria = clock(); */
+
+  /*   printf("==TERMINO DA PESQUISA== \n"); */
+  /*   printf("numero de comparacoes (binario)= %d\n", comparacoes); */
+  /*   if(encontrou != -1) printf("elemento encontrado\nindice = %d\n",encontrou); */
+  /*   else printf("elemento nao encontrado\n"); */
+
+  /*   double tempo_total_binario = ((double) termino_binaria-inicio_binaria)/CLOCKS_PER_SEC; */
+
+  /*   printf("Inicio: %.3lf\nTermino: %.3lf\nTempo de execucao: %.3lf\n\n\n",(double)inicio_binaria/CLOCKS_PER_SEC, (double)termino_binaria/CLOCKS_PER_SEC, tempo_total_binario); */
+
+  /*   comparacoes = 0; */
+  /* } */
+
+  /* free(v_a); */
+
+  // ========================================= //
+
+  /* int * v_b = (int *) malloc (VETOR_B * sizeof(int)); */
+  /* Gerar(v_b, VETOR_B); */
+  /* int tamanho = VETOR_B; */
+  /* int target[6] = {0,49999,99999,149999,199999,-1}; */
+  /* int encontrou=0; */
+  /* int comparacoes = 0; */
+
+  /* merge_sort(v_b,tamanho); */
+
+  /* imprimir_array(v_b, VETOR_B); */
+
+  /* printf("=========== PESQUISAS SEQUENCIAIS==============\n\n\n"); */
+
+  /* for(int i =0; i<6;i++){ */
+  /*   clock_t inicio_sequencial = clock(); */
+  /*   encontrou = PesquisaSequencial(v_b, VETOR_B, target[i], &comparacoes); */
+  /*   clock_t termino_sequencial = clock(); */
+
+  /*   printf("==TERMINO DA PESQUISA== \n"); */
+  /*   printf("numero de comparacoes (sequencial)= %d\n", comparacoes); */
+  /*   if(encontrou != -1) printf("elemento encontrado\nindice = %d\n",encontrou); */
+  /*   else printf("elemento nao encontrado\n"); */
+
+  /*   double tempo_total_sequencial = ((double) termino_sequencial-inicio_sequencial)/CLOCKS_PER_SEC; */
+
+  /*   printf("Inicio: %lf\nTermino: %lf\nTempo de execucao: %lf\n\n\n",(double)inicio_sequencial/CLOCKS_PER_SEC, (double)termino_sequencial/CLOCKS_PER_SEC, tempo_total_sequencial); */
+
+  /*   comparacoes = 0; */
+
+  /* } */
+
+  /* printf("=========== PESQUISAS BINARIA==============\n\n\n"); */
+
+  /* comparacoes = 0; */
+
+  /* for(int i =0; i<6;i++){ */
+
+  /*   clock_t inicio_binaria = clock(); */
+  /*   encontrou = PesquisaBinaria(v_b, VETOR_B, target[i], &comparacoes); */
+  /*   clock_t termino_binaria = clock(); */
+
+  /*   printf("==TERMINO DA PESQUISA== \n"); */
+  /*   printf("numero de comparacoes (binario)= %d\n", comparacoes); */
+  /*   if(encontrou != -1) printf("elemento encontrado\nindice = %d\n",encontrou); */
+  /*   else printf("elemento nao encontrado\n"); */
+
+  /*   double tempo_total_binario = ((double) termino_binaria-inicio_binaria)/CLOCKS_PER_SEC; */
+
+  /*   printf("Inicio: %lf\nTermino: %lf\nTempo de execucao: %lf\n\n\n",(double)inicio_binaria/CLOCKS_PER_SEC, (double)termino_binaria/CLOCKS_PER_SEC, tempo_total_binario); */
+
+  /*   comparacoes = 0; */
+  /* } */
+
+  /* free(v_b); */
+
+
+  // =========================================== //
+
+  int * v_c = (int *) malloc (VETOR_C * sizeof(int));
+  Gerar(v_c, VETOR_C);
+  int tamanho = VETOR_C;
+  int target[6] = {0,74999,149999,224999,299999,-1};
   int encontrou=0;
   int comparacoes = 0;
 
-  merge_sort(v_a,tamanho);
+  merge_sort(v_c,tamanho);
 
-  imprimir_array(v_a, VETOR_A);
+  imprimir_array(v_c, VETOR_C);
 
   printf("=========== PESQUISAS SEQUENCIAIS==============\n\n\n");
 
   for(int i =0; i<6;i++){
     clock_t inicio_sequencial = clock();
-    encontrou = PesquisaSequencial(v_a, VETOR_A, target[i], &comparacoes);
+    encontrou = PesquisaSequencial(v_c, VETOR_C, target[i], &comparacoes);
     clock_t termino_sequencial = clock();
 
     printf("==TERMINO DA PESQUISA== \n");
@@ -71,7 +186,7 @@ int main(){
 
     double tempo_total_sequencial = ((double) termino_sequencial-inicio_sequencial)/CLOCKS_PER_SEC;
 
-    printf("Inicio: %.3lf\nTermino: %.3lf\nTempo de execucao: %.3lf\n\n\n",(double)inicio_sequencial/CLOCKS_PER_SEC, (double)termino_sequencial/CLOCKS_PER_SEC, tempo_total_sequencial);
+    printf("Inicio: %lf\nTermino: %lf\nTempo de execucao: %lf\n\n\n",(double)inicio_sequencial/CLOCKS_PER_SEC, (double)termino_sequencial/CLOCKS_PER_SEC, tempo_total_sequencial);
 
     comparacoes = 0;
 
@@ -84,7 +199,7 @@ int main(){
   for(int i =0; i<6;i++){
 
     clock_t inicio_binaria = clock();
-    encontrou = PesquisaBinaria(v_a, VETOR_A, target[i], &comparacoes);
+    encontrou = PesquisaBinaria(v_c, VETOR_C, target[i], &comparacoes);
     clock_t termino_binaria = clock();
 
     printf("==TERMINO DA PESQUISA== \n");
@@ -94,68 +209,11 @@ int main(){
 
     double tempo_total_binario = ((double) termino_binaria-inicio_binaria)/CLOCKS_PER_SEC;
 
-    printf("Inicio: %.3lf\nTermino: %.3lf\nTempo de execucao: %.3lf\n\n\n",(double)inicio_binaria/CLOCKS_PER_SEC, (double)termino_binaria/CLOCKS_PER_SEC, tempo_total_binario);
+    printf("Inicio: %lf\nTermino: %lf\nTempo de execucao: %lf\n\n\n",(double)inicio_binaria/CLOCKS_PER_SEC, (double)termino_binaria/CLOCKS_PER_SEC, tempo_total_binario);
 
     comparacoes = 0;
   }
 
-  free(v_a);
-
-  // ========================================= //
-
-  /* int * v_b = (int *) malloc (VETOR_B * sizeof(int)); */
-  /* Gerar(v_b, VETOR_B); */
-  /* int tamanho = VETOR_B; */
-  /* int target[6] = {1111381307,84434356,1096188887,846248342,2136419159,73}; */
-  /* int encontrou=0; */
-
-  /* merge_sort(v_b,tamanho); */
-
-  /* imprimir_array(v_b, VETOR_B); */
-
-  /* for(int i =0; i<6;i++){ */
-  /*   clock_t inicio = clock(); */
-  /*   encontrou = PesquisaBinaria(v_b, VETOR_B, target[i]); */
-  /*   clock_t termino = clock(); */
-
-
-  /*   printf("==TERMINO DA PESQUISA== \n"); */
-  /*   if(encontrou != -1) printf("elemento encontrado\nindice = %d\n",encontrou); */
-  /*   else printf("elemento nao encontrado\n"); */
-  /*   double tempo_total = ((double) termino-inicio)/CLOCKS_PER_SEC; */
-  /*   printf("Inicio: %.3lf\nTermino: %.3lf\nTempo de execucao: %.3lf\n\n\n",(double)inicio/CLOCKS_PER_SEC, (double)termino/CLOCKS_PER_SEC, tempo_total); */
-
-  /* } */
-
-  /* free(v_b); */
-
-
-  // =========================================== //
-
-  /* int * v_c = (int *) malloc (VETOR_C * sizeof(int)); */
-  /* Gerar(v_c, VETOR_C); */
-  /* int tamanho = VETOR_C; */
-  /* int target[6] = {2136720149,1749524285,1380923657,545800645,714204102,73}; */
-  /* int encontrou=0; */
-
-  /* merge_sort(v_c,tamanho); */
-
-  /* imprimir_array(v_c, VETOR_C); */
-
-  /* for(int i =0; i<6;i++){ */
-  /*   clock_t inicio = clock(); */
-  /*   encontrou = PesquisaBinaria(v_c, VETOR_C, target[i]); */
-  /*   clock_t termino = clock(); */
-
-
-  /*   printf("==TERMINO DA PESQUISA== \n"); */
-  /*   if(encontrou != -1) printf("elemento encontrado\nindice = %d\n",encontrou); */
-  /*   else printf("elemento nao encontrado\n"); */
-  /*   double tempo_total = ((double) termino-inicio)/CLOCKS_PER_SEC; */
-  /*   printf("Inicio: %.3lf\nTermino: %.3lf\nTempo de execucao: %.3lf\n\n\n",(double)inicio/CLOCKS_PER_SEC, (double)termino/CLOCKS_PER_SEC, tempo_total); */
-
-  /* } */
-
-  /* free(v_c); */
+  free(v_c);
 
 }
