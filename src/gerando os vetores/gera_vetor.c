6201589b85cbfcf055e8f894a2ccd42d9dccd22d
hugo <hugo.resennde1123@gmail.com>
2026-09-19 15:51:07 -0300

    feat: organizando melhor

---
+++ src/gerando os vetores/gera_vetor.c
@@ -0,0 +1,38 @@
+// matricula 974
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define VETOR_A 100000
+#define VETOR_B 200000
+#define VETOR_C 300000
+#define SEED 974
+
+int Gerar(int* v,int n){
+  clock_t inicio = clock();
+  srand(SEED);
+  for(int i = 0; i < n; i++){
+    v[i]=rand();
+    printf("vetor[%d] = [%d] \n",i,v[i]);
+  }
+  clock_t termino = clock();
+  printf("==TERMINO DO VETOR== \n");
+
+  double tempo_total = ((double) termino-inicio)/CLOCKS_PER_SEC;
+  printf("Inicio: %.3lf\nTermino: %.3lf\nTempo de execucao: %.3lf",(double)inicio/CLOCKS_PER_SEC, (double)termino/CLOCKS_PER_SEC, tempo_total);
+
+}
+
+int main(){
+  /* int * V = (int *) malloc (N * sizeof(int)); */
+  /* int * v_a = (int *) malloc (VETOR_A * sizeof(int)); */
+  /* Gerar(v_a, VETOR_A); */
+  /* free(v_a); */
+  /* int * v_b = (int *) malloc (VETOR_B * sizeof(int)); */
+  /* Gerar(v_b, VETOR_B); */
+  /* free(v_b); */
+  int * v_c = (int *) malloc (VETOR_C * sizeof(int));
+  Gerar(v_c, VETOR_C);
+  free(v_c);
+  return 0;
+}
