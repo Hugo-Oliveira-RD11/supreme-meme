7030b5a3c73819d48f425b12818aaaf5de6de0fb
NBnY5A <victor.lito@a.ucb.br>
2026-09-21 22:49:04 -0300

    Update

--- src/graficos/pesquisa_binaria/200.py
+++ src/graficos/pesquisa_binaria/200.py
@@ -7,7 +7,7 @@ y = [0.000001, 0.000001, 0.000001, 0.000001, 0.000001]
 fig, ax = plt.subplots()
 ax.scatter(x, y, color='blue', s=50)
 
-ax.set_title("Pesquisa Sequencial em um vetor de 200 mil elementos")
+ax.set_title("Pesquisa Binária em um vetor de 200 mil elementos")
 ax.set_xlabel("Índice do elemento")
 ax.set_ylabel("Tempo decorrido (segundos)")
 ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
